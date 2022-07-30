4870097e24ca12585686b21f4037c92e26060d55
Steven Xu <stevenxxiu@gmail.com>
2022-07-30 22:27:43 +1000

    feat: move all style components from ~/.config/dogky/dogky.yaml` into `~/.config/dogky/style.css`, to be more flexible

--- src/config.rs
+++ src/config.rs
@@ -7,24 +7,36 @@ use gtk4::prelude::{DisplayExt, MonitorExt, NativeExt, WidgetExt};
 use gtk4::ApplicationWindow;
 use serde_derive::Deserialize;
 
-use crate::serializable_rgba::SerializableRGBA;
-
 #[derive(Deserialize, Clone, Copy)]
-pub struct Config {
+pub struct ConfigProps {
   pub margin: u32,
   pub width: u32,
-  pub bg_color: SerializableRGBA,
+}
+
+pub struct Config {
+  pub config_props: ConfigProps,
+  pub css_bytes: Vec<u8>,
 }
 
 impl Config {
   pub fn load() -> Result<Config, Box<dyn Error>> {
     let xdg_dirs = xdg::BaseDirectories::with_prefix("dogky").unwrap();
+
     let config_path = xdg_dirs.place_config_file("dogky.yaml")?;
     let config_file = File::open(config_path)?;
-    let config: Config = serde_yaml::from_reader(config_file)?;
-    Ok(config)
-  }
+    let config_props: ConfigProps = serde_yaml::from_reader(config_file)?;
 
+    let style_path = xdg_dirs.place_config_file("style.css")?;
+    let css_bytes = std::fs::read(style_path).unwrap();
+
+    Ok(Config {
+      config_props,
+      css_bytes,
+    })
+  }
+}
+
+impl ConfigProps {
   pub fn calculate_size(self: &Self, window: &ApplicationWindow) -> (u32, u32, u32, u32) {
     let surface = window.surface();
     let display = window.display();
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,19 @@ use gtk4::prelude::{ApplicationExt, ApplicationExtManual, GtkWindowExt, WidgetEx
 use gtk4::{Application, ApplicationWindow, Box, CssProvider, Orientation, StyleContext};
 
 mod config;
-mod serializable_rgba;
 
-use crate::config::Config;
+use crate::config::{Config, ConfigProps};
 
 const APP_ID: &str = "org.dogky";
 
-fn load_css(config: &Config) {
+fn load_css(css_bytes: &Vec<u8>) {
   let provider_static = CssProvider::new();
   provider_static.load_from_data(include_bytes!("style.css"));
 
-  let provider_background = CssProvider::new();
-  provider_background.load_from_data(format!("window {{ background-color: {}; }}", config.bg_color).as_bytes());
+  let provider_custom = CssProvider::new();
+  provider_custom.load_from_data(&css_bytes);
 
-  for provider in [&provider_static, &provider_background] {
+  for provider in [&provider_static, &provider_custom] {
     StyleContext::add_provider_for_display(
       &Display::default().expect("Could not connect to a display."),
       provider,
@@ -27,8 +26,8 @@ fn load_css(config: &Config) {
   }
 }
 
-fn build_ui(app: &Application, config: &Config) {
-  let container = Box::new(Orientation::Vertical, config.margin as i32);
+fn build_ui(app: &Application, config_props: &ConfigProps) {
+  let container = Box::new(Orientation::Vertical, config_props.margin as i32);
   let window = ApplicationWindow::builder()
     .application(app)
     .child(&container)
@@ -38,7 +37,7 @@ fn build_ui(app: &Application, config: &Config) {
 
   // Set initial opacity to 0, to avoid flickering when `move_window.sh` runs
   window.set_opacity(0f64);
-  let (monitor_width, monitor_height, window_width, window_height) = config.calculate_size(&window);
+  let (monitor_width, monitor_height, window_width, window_height) = config_props.calculate_size(&window);
   window.set_size_request(window_width as i32, window_height as i32);
 
   let move_window_script_path = std::env::current_exe()
@@ -60,7 +59,7 @@ fn build_ui(app: &Application, config: &Config) {
 fn main() {
   let config = Config::load().unwrap();
   let app = Application::builder().application_id(APP_ID).build();
-  app.connect_startup(move |_| load_css(&config));
-  app.connect_activate(move |app| build_ui(app, &config));
+  app.connect_startup(move |_| load_css(&config.css_bytes));
+  app.connect_activate(move |app| build_ui(app, &config.config_props));
   app.run();
 }
