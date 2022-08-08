8c3691e126597a066928056c220e48e23eb10e34
Steven Xu <stevenxxiu@gmail.com>
2022-08-08 20:22:52 +1000

    fix: use a single `Mutex` for all of graph's data
    There's no need for a `cur_index` either.

--- src/custom_components/graph.rs
+++ src/custom_components/graph.rs
@@ -3,13 +3,17 @@ use gtk::gdk::RGBA;
 use gtk::glib::{Continue, Receiver};
 use gtk::prelude::{DrawingAreaExtManual, WidgetExt};
 use gtk::DrawingArea;
-use std::cell::Cell;
 use std::sync::{Arc, Mutex};
 
 use crate::gtk_utils;
 
 const MAX_NUM_VALUES: usize = 500;
 const BORDER_WIDTH: u32 = 1;
+
+struct GraphData {
+  values: [f32; MAX_NUM_VALUES],
+  next_index: usize,
+}
 
 pub fn build_graph(
   drawing_area: DrawingArea,
@@ -21,28 +25,29 @@ pub fn build_graph(
   let (min_x, max_x, _inner_width, _min_y, max_y, inner_height) =
     gtk_utils::get_drawing_area_inner_dims(&drawing_area, BORDER_WIDTH);
 
-  let values = Arc::new(Mutex::new([0f32; MAX_NUM_VALUES]));
+  let data = Arc::new(Mutex::new(GraphData {
+    values: [0f32; MAX_NUM_VALUES],
+    next_index: 0,
+  }));
   let num_values = (max_x - min_x + 1) as usize;
-  let next_index = Arc::new(Cell::new(0usize));
 
   let [bord_r, bord_g, bord_b, bord_a] = gtk_utils::get_color_components(border_color);
   let [graph_r, graph_g, graph_b, graph_a] = gtk_utils::get_color_components(graph_color);
 
-  let values_ = values.clone();
-  let next_index_ = next_index.clone();
+  let data_ = data.clone();
   drawing_area.set_draw_func(
     move |_drawing_area: &DrawingArea, context: &Context, width: i32, height: i32| {
-      let values_ = values_.lock().unwrap();
+      let data = data_.lock().unwrap();
       // Draw graph
       context.set_source_rgba(graph_r, graph_g, graph_b, graph_a);
-      let mut i = next_index_.get();
+      let mut i = data.next_index;
       let mut x = max_x;
       loop {
         if i == 0 {
           i = num_values;
         }
         i -= 1;
-        let value = values_[i];
+        let value = data.values[i];
         let y_offset = inner_height as f64 * (value / max_value) as f64;
         context.move_to(x as f64, max_y as f64);
         context.line_to(x as f64, max_y as f64 - y_offset);
@@ -59,15 +64,15 @@ pub fn build_graph(
     },
   );
 
-  let values_ = values.clone();
-  let next_index_ = next_index.clone();
+  let data_ = data.clone();
   receiver.attach(None, move |cur_value| {
-    let mut values_ = values_.lock().unwrap();
-    values_[next_index_.get()] = cur_value;
+    let mut data = data_.lock().unwrap();
+    let next_index = data.next_index;
+    data.values[next_index] = cur_value;
 
-    next_index_.set(next_index_.get() + 1);
-    if next_index_.get() >= num_values {
-      next_index_.set(0);
+    data.next_index += 1;
+    if data.next_index >= num_values {
+      data.next_index = 0;
     }
 
     drawing_area.queue_draw();
