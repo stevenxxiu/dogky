e4a6620819be07f13427fdf8d65b56795526be1f
Steven Xu <stevenxxiu@gmail.com>
2025-09-18 01:47:01 +1000

    fix(move-window): check if *Waybar* is active using container heights, to work with and without *Waybar*

--- src/move_window.py
+++ src/move_window.py
@@ -4,12 +4,48 @@ import inspect
 import json
 import subprocess
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Self, override
 
 
-class ParsedArgs(argparse.Namespace):
-    window_width: int  # pyright: ignore [reportUninitializedInstanceVariable]
-    window_height: int  # pyright: ignore [reportUninitializedInstanceVariable]
+class FromDictMixin:
+    @classmethod
+    def from_dict(cls, env: dict[str, Any]) -> Self:  # pyright: ignore[reportExplicitAny]
+        return cls(
+            **{
+                k: v
+                for k, v in env.items()  # pyright: ignore[reportAny]
+                if k in inspect.signature(cls).parameters
+            }
+        )
+
+
+@dataclass
+class Rect:
+    x: int
+    y: int
+    width: int
+    height: int
+
+
+@dataclass
+class Workspace(FromDictMixin):
+    rect: Rect
+    focused: bool
+
+    @classmethod
+    @override
+    def from_dict(cls, env: dict[str, Any]) -> Self:  # pyright: ignore[reportExplicitAny]
+        env['rect'] = Rect(**env['rect'])  # pyright: ignore[reportAny]
+        return super().from_dict(env)
+
+
+def get_workspace_rect() -> Rect:
+    workspaces_str = subprocess.check_output(['swaymsg', '--type', 'get_workspaces'], encoding='utf-8')
+    workspaces = [Workspace.from_dict(obj) for obj in json.loads(workspaces_str)] # pyright: ignore[reportAny]
+    for workspace in workspaces:
+        if workspace.focused:
+            return workspace.rect
+    raise ValueError
 
 
 @dataclass
@@ -21,25 +57,25 @@ class CurrentMode:
 
 
 @dataclass
-class Output:
+class Output(FromDictMixin):
     scale: float
     current_mode: CurrentMode
     focused: bool
 
     @classmethod
-    def from_dict(cls, env: dict[str, Any]):  # pyright: ignore[reportExplicitAny]
+    @override
+    def from_dict(cls, env: dict[str, Any]) -> Self:  # pyright: ignore[reportExplicitAny]
         env['current_mode'] = CurrentMode(**env['current_mode'])  # pyright: ignore[reportAny]
-        return cls(
-            **{  # pyright: ignore[reportAny]
-                k: v
-                for k, v in env.items()  # pyright: ignore[reportAny]
-                if k in inspect.signature(cls).parameters
-            }
-        )
+        return super().from_dict(env)
 
 
-WAYBAR_HEIGHT = 32
+class ParsedArgs(argparse.Namespace):
+    window_width: int  # pyright: ignore [reportUninitializedInstanceVariable]
+    window_height: int  # pyright: ignore [reportUninitializedInstanceVariable]
+
+
 WINDOW_CRITERIA = '[app_id="dogky"]'
+
 
 def main() -> None:
     arg_parser = argparse.ArgumentParser(description='Move the *Dogky* window in position.')
@@ -49,19 +85,22 @@ def main() -> None:
     outputs_str = subprocess.check_output(['swaymsg', '--type', 'get_outputs'], encoding='utf-8')
     outputs = [Output.from_dict(obj) for obj in json.loads(outputs_str)] # pyright: ignore[reportAny]
 
-    width, height = 0, 0
+    output_width, output_height = 0, 0
     for output in outputs:
         if output.focused:
-            width, height = output.current_mode.width, output.current_mode.height
-            width = round(width / output.scale)
-            height = round(height / output.scale)
+            output_width, output_height = output.current_mode.width, output.current_mode.height
+            output_width = round(output_width / output.scale)
+            output_height = round(output_height / output.scale)
 
-    height -= WAYBAR_HEIGHT
-    pos_x = width - args.window_width
-    pos_y = WAYBAR_HEIGHT
+    workspace_rect = get_workspace_rect()
+    bars_height = output_height - workspace_rect.height # Possibly 0
+
+    output_height -= bars_height
+    pos_x = output_width - args.window_width
+    pos_y = bars_height
 
     _ = subprocess.check_call(['swaymsg', ';'.join([
-        f'for_window {WINDOW_CRITERIA} resize set {args.window_width} {height}',
+        f'for_window {WINDOW_CRITERIA} resize set {args.window_width} {output_height}',
         f'for_window {WINDOW_CRITERIA} move absolute position {pos_x} {pos_y}',
     ])])
 
