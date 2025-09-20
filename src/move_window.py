b611e1dd2b675573eaa20f90d1d37db14319a89d
Steven Xu <stevenxxiu@gmail.com>
2025-09-20 17:46:40 +1000

    refactor(move-window): use `TypedDict` for JSON

--- src/move_window.py
+++ src/move_window.py
@@ -1,72 +1,43 @@
 #!/usr/bin/env python
 import argparse
-import inspect
 import json
 import subprocess
-from dataclasses import dataclass
-from typing import Any, Self, override
+from typing import TypedDict
 
 
-class FromDictMixin:
-    @classmethod
-    def from_dict(cls, env: dict[str, Any]) -> Self:  # pyright: ignore[reportExplicitAny]
-        return cls(
-            **{
-                k: v
-                for k, v in env.items()  # pyright: ignore[reportAny]
-                if k in inspect.signature(cls).parameters
-            }
-        )
-
-
-@dataclass
-class Rect:
+class Rect(TypedDict):
     x: int
     y: int
     width: int
     height: int
 
 
-@dataclass
-class Workspace(FromDictMixin):
+class Workspace(TypedDict):
     rect: Rect
     focused: bool
-
-    @classmethod
-    @override
-    def from_dict(cls, env: dict[str, Any]) -> Self:  # pyright: ignore[reportExplicitAny]
-        env['rect'] = Rect(**env['rect'])  # pyright: ignore[reportAny]
-        return super().from_dict(env)
 
 
 def get_workspace_rect() -> Rect:
     workspaces_str = subprocess.check_output(['swaymsg', '--type', 'get_workspaces'], encoding='utf-8')
-    workspaces = [Workspace.from_dict(obj) for obj in json.loads(workspaces_str)] # pyright: ignore[reportAny]
+    workspaces: list[Workspace] = json.loads(workspaces_str)  # pyright: ignore[reportAny]
     for workspace in workspaces:
-        if workspace.focused:
-            return workspace.rect
+        if workspace['focused']:
+            rect: Rect = workspace['rect']
+            return rect
     raise ValueError
 
 
-@dataclass
-class CurrentMode:
+class CurrentMode(TypedDict):
     width: int
     height: int
     refresh: int
     picture_aspect_ratio: str
 
 
-@dataclass
-class Output(FromDictMixin):
+class Output(TypedDict):
     scale: float
     current_mode: CurrentMode
     focused: bool
-
-    @classmethod
-    @override
-    def from_dict(cls, env: dict[str, Any]) -> Self:  # pyright: ignore[reportExplicitAny]
-        env['current_mode'] = CurrentMode(**env['current_mode'])  # pyright: ignore[reportAny]
-        return super().from_dict(env)
 
 
 class ParsedArgs(argparse.Namespace):
@@ -83,17 +54,16 @@ def main() -> None:
     args = arg_parser.parse_args(namespace=ParsedArgs)
 
     outputs_str = subprocess.check_output(['swaymsg', '--type', 'get_outputs'], encoding='utf-8')
-    outputs = [Output.from_dict(obj) for obj in json.loads(outputs_str)] # pyright: ignore[reportAny]
+    outputs: list[Output] = json.loads(outputs_str) # pyright: ignore[reportAny]
 
     output_width, output_height = 0, 0
     for output in outputs:
-        if output.focused:
-            output_width, output_height = output.current_mode.width, output.current_mode.height
-            output_width = round(output_width / output.scale)
-            output_height = round(output_height / output.scale)
+        if output['focused']:
+            output_width = round(output['current_mode']['width'] / output['scale'])
+            output_height = round(output['current_mode']['height'] / output['scale'])
 
     workspace_rect = get_workspace_rect()
-    bars_height = output_height - workspace_rect.height # Possibly 0
+    bars_height = output_height - workspace_rect['height'] # Possibly 0
 
     output_height -= bars_height
     pos_x = output_width - args.window_width
